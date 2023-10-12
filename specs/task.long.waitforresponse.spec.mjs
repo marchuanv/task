41c8a6f7d687ea58b4c1c9bf58840947bc29b68e
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-10-12 09:26:04 +0200

    refactor

--- specs/task.long.waitforresponse.spec.mjs
+++ specs/task.long.waitforresponse.spec.mjs
@@ -3,12 +3,14 @@ import { TaskState } from '../lib/task-state.mjs';
 import { Task } from '../task.mjs';
 fdescribe('when queueing tasks given a wait for response flag on long running tasks', () => {
     it('should wait for response', async () => {
+
         let executedTasks = [];
         let isRepeatTaskALongRunning = false;
         let isRepeatTaskBLongRunning = false;
         let isRepeatTaskCLongRunning = false;
-        const block = new Promise((resolve) => setTimeout(resolve, 3000));
-        const taskAResponse = await Task.create('WaitLongTaskA', { Id: 'WaitLongTaskAId' }, {}, [TaskFlag.WaitForValidResponse]).queue(Object.prototype, async function () {
+
+        const block = new Promise((resolve) => setTimeout(resolve, 2000));
+        const taskAPromise = Task.create('WaitLongTaskA', { Id: 'WaitLongTaskAId' }, {}, [TaskFlag.WaitForValidResponse]).queue(Object.prototype, async function () {
             await block;
             executedTasks.push(this);
             setTimeout(() => {
@@ -18,7 +20,7 @@ fdescribe('when queueing tasks given a wait for response flag on long running ta
                 }
             }, 1000);
         });
-        const taskBResponse = await Task.create('WaitLongTaskB', { Id: 'WaitLongTaskBId' }, {}, [TaskFlag.WaitForValidResponse]).queue(Object.prototype, async function () {
+        const taskBPromise = Task.create('WaitLongTaskB', { Id: 'WaitLongTaskBId' }, {}, [TaskFlag.WaitForValidResponse]).queue(Object.prototype, async function () {
             await block;
             executedTasks.push(this);
             setTimeout(() => {
@@ -28,7 +30,7 @@ fdescribe('when queueing tasks given a wait for response flag on long running ta
                 }
             }, 1000);
         });
-        const taskCResponse = await Task.create('WaitLongTaskC', { Id: 'WaitLongTaskCId' }, {}, [TaskFlag.WaitForValidResponse]).queue(Object.prototype, async function () {
+        const taskCPromise = Task.create('WaitLongTaskC', { Id: 'WaitLongTaskCId' }, {}, [TaskFlag.WaitForValidResponse]).queue(Object.prototype, async function () {
             await block;
             executedTasks.push(this);
             setTimeout(() => {
@@ -38,15 +40,17 @@ fdescribe('when queueing tasks given a wait for response flag on long running ta
                 }
             }, 1000);
         });
-        setTimeout(() => {
-            expect(executedTasks.length).toBeGreaterThan(20);
-            expect(taskAResponse).toBeDefined();
-            expect(taskBResponse).toBeDefined();
-            expect(taskCResponse).toBeDefined();
-            expect(isRepeatTaskALongRunning).toBeTrue();
-            expect(isRepeatTaskBLongRunning).toBeTrue();
-            expect(isRepeatTaskCLongRunning).toBeTrue();
-            done();
-        }, 1000)
+
+        const taskAResponse = await taskAPromise;
+        const taskBResponse = await taskBPromise;
+        const taskCResponse = await taskCPromise;
+
+        expect(taskAResponse).toBeDefined();
+        expect(taskBResponse).toBeDefined();
+        expect(taskCResponse).toBeDefined();
+        expect(isRepeatTaskALongRunning).toBeTrue();
+        expect(isRepeatTaskBLongRunning).toBeTrue();
+        expect(isRepeatTaskCLongRunning).toBeTrue();
+
     });
 });
