18480d0d5040875a1a86c0e28d3c481d1cd80382
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-08 16:56:32 +0200

    refactor

--- specs/task-promise/task.repeat.promise.resolve.with.data.spec.mjs
+++ specs/task-promise/task.repeat.promise.resolve.with.data.spec.mjs
@@ -4,17 +4,26 @@ import { TaskProperties } from '../../lib/task-properties.mjs';
 const suite = fdescribe('when resolving a promise for a repeated task given data', () => {
     it('should resolve with data', async () => {
         const taskProperties = new TaskProperties('TaskPromiseTestRepeat', { Id: 'TaskPromiseTestRepeat' }, {}, 1000);
-        const expectedMessage = { message: 'TaskPromiseTestRepeat' };
-        let count = 1;
-        setTimeout(() => {
-            while (count <= 5) {
-                expectedMessage.message = `TaskPromiseTestRepeat${count}`;
-                let _taskPromise = new TaskPromise(taskProperties);
-                taskProperties.data = expectedMessage;
-                _taskPromise.resolve();
-                count = count + 1;
-            }
-        }, 3000);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat1' });
+        }, 100);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat2' });
+        }, 100);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat3' });
+        }, 100);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat4' });
+        }, 100);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat5' });
+        }, 100);
         const results = await taskProperties.promise.get();
         expect(results).toBeDefined();
         expect(results).not.toBeNull();
