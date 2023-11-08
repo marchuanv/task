6ea26d8b1d1f72325a0a5b148e677ff61b893d31
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-08 19:23:31 +0200

    refactor

--- specs/task-promise/task.repeat.promise.resolve.with.data.spec.mjs
+++ specs/task-promise/task.repeat.promise.resolve.with.data.spec.mjs
@@ -3,27 +3,38 @@ import { TaskPromise } from '../../lib/task-promise.mjs';
 import { TaskProperties } from '../../lib/task-properties.mjs';
 const suite = fdescribe('when resolving a promise for a repeated task given data', () => {
     it('should resolve with data', async () => {
-        const taskProperties = new TaskProperties('TaskPromiseTestRepeat', { Id: 'TaskPromiseTestRepeat' }, {}, 1000);
-        setTimeout(async () => {
-            let _taskPromise = new TaskPromise(taskProperties);
-            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat1' });
-        }, 100);
-        setTimeout(async () => {
-            let _taskPromise = new TaskPromise(taskProperties);
-            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat2' });
-        }, 100);
-        setTimeout(async () => {
-            let _taskPromise = new TaskPromise(taskProperties);
-            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat3' });
-        }, 100);
-        setTimeout(async () => {
-            let _taskPromise = new TaskPromise(taskProperties);
-            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat4' });
-        }, 100);
+        const taskProperties = new TaskProperties('TaskPromiseTestRepeat', {
+            Id: 'TaskPromiseTestRepeat'
+        }, {}, 1000);
         setTimeout(async () => {
             let _taskPromise = new TaskPromise(taskProperties);
             _taskPromise.resolve({ message: 'TaskPromiseTestRepeat5' });
+            console.log('TaskPromiseTestRepeat5');
         }, 100);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat1' });
+            console.log('TaskPromiseTestRepeat1');
+        }, 150);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat2' });
+            console.log('TaskPromiseTestRepeat2');
+        }, 300);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat4' });
+            console.log('TaskPromiseTestRepeat4');
+        }, 310);
+        setTimeout(async () => {
+            let _taskPromise = new TaskPromise(taskProperties);
+            _taskPromise.resolve({ message: 'TaskPromiseTestRepeat3' });
+            console.log('TaskPromiseTestRepeat3');
+        }, 300);
+        setTimeout(async () => {
+            taskProperties.promise.resolve({ message: 'TaskPromiseTestRepeat' });
+            console.log('TaskPromiseTestRepeat');
+        }, 400);
         const results = await taskProperties.promise.get();
         expect(results).toBeDefined();
         expect(results).not.toBeNull();
